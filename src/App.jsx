cdce8d1e4fc80984a4ee124eef63fae95140d4ea
Bayzid Miah <sejan.bayzid@gmail.com>
2025-06-07 10:56:37 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+
+const App = () => {
+  return (
+    <>
+      
+      <Sidebar />
+
+    </>
+  )
+}
+
+export default App
